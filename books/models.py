fc85071e8b461fa549b3ec2a5599d9b2a220cc6f
entirelymagic <entirelymagic@gmail.com>
2021-08-02 11:16:43 +0300

    build(Add  category and books):
    Categories have a name
    Books have:
    - title
    - author
    - category
    - created_at
    -updated_at
    
    Reviewed-by: Elvis
    Refs #3

--- books/models.py
+++ books/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+
+class BooksCategory(models.Model):
+    """A category class for books"""
+    name = models.CharField(max_length=100)
+
+class Books(models.Model):
+    """ A book class representation"""
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    category = models.ForeignKey(BooksCategory, on_delete=models.CASCADE)
+    release_date = models.DateField(auto_now_add=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+    
